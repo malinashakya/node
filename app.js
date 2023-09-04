176d10b306e2c8a69689b08158248df561aa8ccb
Malina <malinashakya72@gmail.com>
2023-09-03 22:01:03 -0700

    Learned about ejs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express= require("express")//requiring express package
 const app=express() //storing it in app, app vaneko variable throughout project use garxam
 
+//I'm using ejs what you have to perform do it.
+app.set("view engine","ejs")
+
+//If we used pug instead of ejs
+//app.set("view engine","pug")
 //localhost:3000/ halda aaune output
 app.get('/',(req,res)=>
 {
-    res.send("<h1>hello world</h1>")
+    // res.send("<h1>hello worldheheabba!!</h1>")
+   //home.ejs page ma lagdeu vaneko
+    res.render("home.ejs")
 })
 
 //localhost:3000/contact halda aaune output
@@ -19,7 +26,7 @@ app.get('/contact',(req,res)=>
 //localhost:3000/about halda aaune output
 app.get('/about',(req,res)=>
 {
-    res.send("about page")
+    res.render("about.ejs")
 }
 )
 //port number-> room number jastai
